801f4071683ed0fbf4372b581506731da86cf51c
Erik Luebbers <erluebbers@gmail.com>
2022-04-04 05:53:13 -0700

    add create crud

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,13 @@
 import '../App.css';
-import React from "react";
+import React, { useState } from "react";
 
 
 function Intro( {theaterInfo, handleClosure} ) {
+  const [formData, setFormData] = useState({
+    name: "",
+    opening_date: 2022,
+    rating: ""
+  })
 
   const theaterList = theaterInfo.map(theater => {
     return (
@@ -11,6 +16,26 @@ function Intro( {theaterInfo, handleClosure} ) {
         <button onClick={() => handleClosure(theater)}>Report Closure</button>
       </div>)
   })
+
+  const handleCreate = (event) => {
+    event.preventDefault()
+    fetch(`http://localhost:9292/movies`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formData)
+    })
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name
+    let value = event.target.value
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  console.log(formData)
 
 
   return (
@@ -21,6 +46,23 @@ function Intro( {theaterInfo, handleClosure} ) {
       <ul>
         {theaterList}
       </ul>
+      <h3>Have you noticed a new movie playing at one of our local theaters? Report it using the below form to keep other users updated!</h3>
+      <form onSubmit={handleCreate}>
+        <input 
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+        <select onChange={handleChange} name="rating">
+          <option value="NC-17">NC-17</option>
+          <option value="R">R</option>
+          <option value="PG-13">PG-13</option>
+          <option value="PG">PG</option>
+          <option value="G">G</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
